69c795963249c9663db564f90fb1709425ec3d73
Vaishak Shetty <81238186+VaishShetty@users.noreply.github.com>
2021-06-29 21:36:06 +0530

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 10 20:20:56 2021
 
-@author: Vaishak
-"""
-
-
+## Import required modules/packages
 from pixellib.semantic import semantic_segmentation
 import numpy as np
 import cv2
@@ -16,9 +11,11 @@ def load_images_from_folder(folder):
     '''
     Description : Loads the images from the given directory 
     
-    Input argument(s) : folder - Input directory path to input images. Type - str 
+    Input argument(s) : 
+    1. folder (type = str) - Input directory path to input images. 
     
-    Returns : images - List of image 
+    Returns :
+    1. images (type = list) - List of image 
     '''
     images = []
     for filename in os.listdir(folder):
@@ -27,27 +24,33 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
-
-
-## Main Function 
+## Main/Driver Function 
 if __name__ == "__main__":
-        
-    imagePath = 'Input Images'
+    '''
+    Description : Driver function or Entry point of the program. 
+    '''
     
+    # Load Input images 
+    imagePath = 'Input Images'
     inputImagesColor = load_images_from_folder(imagePath)
     
+    # Process each image
     for iterVar in range(len(inputImagesColor)):
-            
+        
+        # Segment image 
         segment_image = semantic_segmentation()
         segment_image.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5") 
         segmap, segoverlay = segment_image.segmentAsPascalvoc(inputImagesColor[iterVar],process_frame = True)
-            
+         
+        # Post-Processing on image
         gray = cv2.cvtColor(segoverlay, cv2.COLOR_BGR2GRAY)
         ret2,Mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         
+        # Create a mask from the replace background pixels with the grayscale value "255"
         SegOutput = cv2.bitwise_and(inputImagesColor[iterVar], inputImagesColor[iterVar], mask=Mask)
-        
         black=np.where((SegOutput[:,:,0]==0) & (SegOutput[:,:,1]==0) & (SegOutput[:,:,2]==0))
         SegOutput[black]=(255,255,255)
+        
+        # Write output image
         cv2.imwrite("Output images\SegOutput"+ str(iterVar)+".jpg", SegOutput)
         
