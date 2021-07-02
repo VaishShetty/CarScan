da00ae417261ad16fe231f8594086f48ef480d86
Vaishak Shetty <81238186+VaishShetty@users.noreply.github.com>
2021-07-02 20:06:26 +0530

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -42,20 +42,20 @@ if __name__ == "__main__":
         segment_image = semantic_segmentation()
         segment_image.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
         process_frame = True
-        segmap, segoverlay = segment_image.segmentAsPascalvoc(image, process_frame)
+        segmap, seg_overlay = segment_image.segmentAsPascalvoc(image, process_frame)
 
         # Post-Processing on image
-        gray = cv2.cvtColor(segoverlay, cv2.COLOR_BGR2GRAY)
-        ret2, Mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        gray = cv2.cvtColor(seg_overlay, cv2.COLOR_BGR2GRAY)
+        ret2, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-        # Create a mask from the replace background pixels with the grayscale value "255"
-        SegOutput = cv2.bitwise_and(image, image, mask=Mask)
-        b_plane = (SegOutput[:, :, 2] == 0)
-        g_plane = (SegOutput[:, :, 1] == 0)
-        r_plane = (SegOutput[:, :, 0] == 0)
+        # Create a mask to the replace background pixels with the grayscale value "255"
+        seg_output = cv2.bitwise_and(image, image, mask=Mask)
+        b_plane = (seg_output[:, :, 2] == 0)
+        g_plane = (seg_output[:, :, 1] == 0)
+        r_plane = (seg_output[:, :, 0] == 0)
         black = np.where(r_plane & g_plane & b_plane)
-        SegOutput[black] = (255, 255, 255)
+        seg_output[black] = (255, 255, 255)
 
         # Write output image
-        cv2.imwrite("Output images/SegOutput"+ str(idx)+".jpg", SegOutput)
+        cv2.imwrite("Output images/seg_output"+ str(idx)+".jpg", seg_output)
         
