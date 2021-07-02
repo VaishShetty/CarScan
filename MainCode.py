f6c91782df9b34d07d7e019873de3cfc0bf6d20a
Vaishak <vaishakshetty1@gmail.com>
2021-07-02 20:03:44 +0530

    Improvement using PEP-8 standards

--- MainCode.py
+++ MainCode.py
@@ -1,56 +1,61 @@
 # -*- coding: utf-8 -*-
+"""
+Modified on Fri Jul  2 19:55:47 2021
+
+Author: Vaishak
+
+Descrption : This code demonstrates the object segmentation using a pre-trained model.
+"""
 
 ## Import required modules/packages
-from pixellib.semantic import semantic_segmentation
-import numpy as np
-import cv2
 import os
+import numpy as np
+from cv2 import cv2
+from pixellib.semantic import semantic_segmentation
 
-## Load images from the folder/direcotry 
-def load_images_from_folder(folder):
+def load_images_from_folder(folder)->list:
     '''
-    Description : Loads the images from the given directory 
-    
-    Input argument(s) : 
-    1. folder (type = str) - Input directory path to input images. 
-    
-    Returns :
-    1. images (type = list) - List of image 
+    Description : Loads the images from the given directory
+
+    Input argument(s) : folder (type = str) - Input directory path to input images
+
+    Returns : images (type = list) - List of image
     '''
     images = []
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
+        img = cv2.imread(os.path.join(folder, filename))
         if img is not None:
             images.append(img)
     return images
 
-## Main/Driver Function 
+## Main/Driver Function
 if __name__ == "__main__":
-    '''
-    Description : Driver function or Entry point of the program. 
-    '''
-    
-    # Load Input images 
-    imagePath = 'Input Images'
-    inputImagesColor = load_images_from_folder(imagePath)
-    
+
+    # Load Input images
+    IMAGEPATH = 'Input Images'
+    COLORIMAGELIST = load_images_from_folder(IMAGEPATH)
+
     # Process each image
-    for iterVar in range(len(inputImagesColor)):
-        
-        # Segment image 
+    for idx, image in enumerate(COLORIMAGELIST):
+
+        # Segment image
         segment_image = semantic_segmentation()
-        segment_image.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5") 
-        segmap, segoverlay = segment_image.segmentAsPascalvoc(inputImagesColor[iterVar],process_frame = True)
-         
+        segment_image.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+        process_frame = True
+        segmap, segoverlay = segment_image.segmentAsPascalvoc(image, process_frame)
+
         # Post-Processing on image
         gray = cv2.cvtColor(segoverlay, cv2.COLOR_BGR2GRAY)
-        ret2,Mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        
+        ret2, Mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
         # Create a mask from the replace background pixels with the grayscale value "255"
-        SegOutput = cv2.bitwise_and(inputImagesColor[iterVar], inputImagesColor[iterVar], mask=Mask)
-        black=np.where((SegOutput[:,:,0]==0) & (SegOutput[:,:,1]==0) & (SegOutput[:,:,2]==0))
-        SegOutput[black]=(255,255,255)
-        
+        SegOutput = cv2.bitwise_and(image, image, mask=Mask)
+        b_plane = (SegOutput[:, :, 2] == 0)
+        g_plane = (SegOutput[:, :, 1] == 0)
+        r_plane = (SegOutput[:, :, 0] == 0)
+        black = np.where(r_plane & g_plane & b_plane)
+        SegOutput[black] = (255, 255, 255)
+
         # Write output image
-        cv2.imwrite("Output images\SegOutput"+ str(iterVar)+".jpg", SegOutput)
+        cv2.imwrite("Output images/SegOutput"+ str(idx)+".jpg", SegOutput)
         
